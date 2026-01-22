bdd1e3a66e2f53c7e6dfa415f142429deffb0c51
justinm1997 <140605817+justinm1997@users.noreply.github.com>
2026-01-22 12:15:51 -0600

    madlibs

---
+++ madlibs.py
@@ -0,0 +1,30 @@
+"""
+-----------------------------------------------------------------------
+ASSIGNMENT REQUIREMENTS - Madlibs
+-----------------------------------------------------------------------
+[ ] 1. Header Docstring included (Assignment Name, Date, File Name).
+[ ] 2. Program asks for at least 5 different inputs (variables).
+[ ] 3. Output uses F-Strings to combine text and variables.
+[ ] 4. Output uses at least one escape sequence (\n or \t).
+[ ] 5. Code contains comments explaining the steps.
+[ ] 6. Program runs without errors.
+-----------------------------------------------------------------------
+"""
+# Mary had a little lamb 
+
+
+
+print("\n\n\nMary had a little lamb")
+print("         By: Saeah Josepha Hale")
+
+# Variables
+person_name = input("Person's name:   ")
+animal = input("Type of animal:   ")
+color = input("Color:   ")
+
+# Output
+
+print(f"{person_name}) had a little {animal}")
+print(f"Whose fleece was {color} as snow")
+
+
---
+++ madlibsv2.py
@@ -0,0 +1,24 @@
+"""
+-----------------------------------------------------------------------
+ASSIGNMENT REQUIREMENTS - Madlibs
+-----------------------------------------------------------------------
+[ ] 1. Header Docstring included (Assignment Name, Date, File Name).
+[ ] 2. Program asks for at least 5 different inputs (variables).
+[ ] 3. Output uses F-Strings to combine text and variables.
+[ ] 4. Output uses at least one escape sequence (\n or \t).
+[ ] 5. Code contains comments explaining the steps.
+[ ] 6. Program runs without errors.
+-----------------------------------------------------------------------
+"""
+
+print("\n\n\nJustin Loves to play video games")
+print("          By Justin Moravec")
+
+#   Variables 
+
+players name = input("Player's name:   ")
+age = input(":   ")
+
+
+
+#   Outputs
