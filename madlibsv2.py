1d8ecefa7787f136d6a0e947d58ceb8d184489c2
justinm1997 <140605817+justinm1997@users.noreply.github.com>
2026-01-22 12:53:17 -0600

    Update madlibsv2.py

--- madlibsv2.py
+++ madlibsv2.py
@@ -22,3 +22,38 @@ age = input(":   ")
 
 
 #   Outputs
+
+"""
+-----------------------------------------------------------------------
+ASSIGNMENT REQUIREMENTS - Madlibs
+-----------------------------------------------------------------------
+[ ] 1. Header Docstring included (Assignment Name, Date, File Name).
+[ ] 2. Program asks for at least 5 different inputs (variables).
+[ ] 3. Output uses F-Strings to combine text and variables.
+[ ] 4. Output uses at least one escape sequence (\n or \t).
+[ ] 5. Code contains comments explaining the steps.
+[ ] 6. Program runs without errors.
+-----------------------------------------------------------------------
+"""
+
+# Justin loves to play video games
+
+print("\n\n\nJustin loves to play video games")
+
+#   Variables 
+
+players_name = input("Player's name:   ")
+activity = input("Activity name:   ")
+verb = input("Verb:   ")
+noun = input("Noun:   ")
+preposition = input("Preposition:   ")
+
+
+#   Outputs
+
+print(f"{players_name} {verb} {preposition} {noun} {activity}")
+
+
+
+
+
