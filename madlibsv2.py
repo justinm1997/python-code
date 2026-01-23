72a9d2e280a1c3bab00d90b5923cc15ead0dcd19
justinm1997 <140605817+justinm1997@users.noreply.github.com>
2026-01-22 19:48:39 -0600

    Update madlibsv2.py

--- madlibsv2.py
+++ madlibsv2.py
@@ -14,25 +14,26 @@ ASSIGNMENT REQUIREMENTS - Madlibs
 
 # Justin loves to play video games
 
-print("\n\n\nJustin loves to play video games especially Call Of Duty untill Mid-Night.")
+print("\n\n\nJustin loves to play video games especially Call Of Duty until Mid-Night.")
 print("Justin is 28 years old and also enjoys to Work out.")
 print("Justin is usually in a great mood every day.")
 
 #   Variables 
 
 person_name =  input("Person's name:   ")
-videogames = input("Videogame's name    ")
-time_of_day = input("Time of day(Morning, Afternoon, Mid-Night)    ")
-person_age = input("Your Age   ")
-activity = input("What do you like to do in your spare time?    ")
-mood = input("Your mood    ")
+videogames = input("Videogame's name:    ")
+time_of_day = input("Time of day(Morning, Afternoon, Mid-Night):    ")
+person_age = input("Your age:   ")
+activity = input("What do you like to do in your spare time:   ")
+mood = input("Your mood?:    ")
 
 
 #   Outputs
 
-print(f"\n\n\n{person_name} loves to play video games especially {videogames} untill {time_of_day}.")
+print(f"\n\n\n{person_name} loves to play video games especially {videogames} until {time_of_day}.")
 print(f"{person_name} is {person_age} years old and also enjoys to {activity}.")
-print(f"{person_name} is usually in a {mood} mood every day. ")
+print(f"{person_name} is usually in a {mood} mood every day.")
+
 
 
 
