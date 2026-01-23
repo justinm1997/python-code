149737ad4239dd45b18fd86996b1ddbf10773dfe
justinm1997 <140605817+justinm1997@users.noreply.github.com>
2026-01-22 21:29:37 -0600

    Add files via upload

---
+++ hero.py
@@ -0,0 +1,43 @@
+"""
+-----------------------------------------------------------------------
+ASSIGNMENT REQUIREMENTS (DO NOT DELETE)
+-----------------------------------------------------------------------
+Please ensure your code meets the following before submitting:
+
+[ ] 1. Header Docstring included.
+[ ] 2. Define a String variable (e.g., Character Name).
+[ ] 3. Define an Integer variable (e.g., Level or Age).
+[ ] 4. Define a Float variable (e.g., Speed or Power).
+[ ] 5. Define a Boolean variable (e.g., is_alive).
+[ ] 6. Print all variables to the screen using F-Strings.
+[ ] 7. Upload to GitHub and add instructor as a contributor.
+-----------------------------------------------------------------------
+
+"""
+
+# Variables
+# Title
+
+
+print("\n\n\nJustin Myers")
+print("Characters level: 100")
+print("Characters finesse: 88.5")
+print("Justin myers is Alive")
+
+# Details
+
+characters_name = input("Your characters name:   ")
+characters_level = input("Your characters level is:    ")
+characters_finesse = input("Your characters finesse is:    ")
+alive = True
+
+
+# outputs
+
+print(f"\n\n\n{characters_name} Myers")
+print(f"Characters level: {characters_level} ")
+print(f"Characters finesse: {characters_finesse}")
+print(f"{characters_name} myers is alive: {alive}")
+
+
+
