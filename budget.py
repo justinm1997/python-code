f80cfaf0fb3eaa2edc25214999ba32da542c61d6
Justin Morravec <justinmoravec97@yahoo.com>
2026-01-27 11:58:17 -0600

    assignment

---
+++ budget.py
@@ -0,0 +1,12 @@
+"""
+-----------------------------------------------------------------------
+ASSIGNMENT REQUIREMENTS
+-----------------------------------------------------------------------
+[ ] 1. Header Docstring included.
+[ ] 2. Ask user for Monthly Income (float).
+[ ] 3. Ask user for 5 DIFFERENT expense amounts (float).
+[ ] 4. Calculate Total Expenses and Remaining Balance.
+[ ] 5. Calculate Percentage of Income Spent.
+[ ] 6. Output formatted to 2 decimal places (:,.2f).
+-----------------------------------------------------------------------
+"""
