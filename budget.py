1aa25fafff1a2a8eb59f880ea02ee9ca318fd7f7
Justin Morravec <justinmoravec97@yahoo.com>
2026-01-27 12:37:17 -0600

    updatessss

--- budget.py
+++ budget.py
@@ -10,16 +10,11 @@ ASSIGNMENT REQUIREMENTS
 [ ] 6. Output formatted to 2 decimal places (:,.2f).
 -----------------------------------------------------------------------
 """
-
-
-
-# Inputs
-
-print("\n\n\n What's your monthly Income?   ")
-print("What's 5 different expense amounts?  ")
+print("What is your monthly income")
 
 
 # variables
+
 Groceries = float(input("Amount spent on groceries?    "))
 Monthly_memberships = float(input("Amount spent on monthly memberships?   "))
 Pets = float(input("Amount spent on pets?    "))
@@ -29,6 +24,12 @@ Monthly_bills = float(input("Amount spent on monthly bills?   "))
 
 
 # Calculations
+
+spent = Groceries + Monthly_memberships + Monthly_bills + video_games + Pets
+
+remaining = Groceries - Monthly_memberships - Monthly_bills - video_games - Pets
+
+percent = 
 
 
 
