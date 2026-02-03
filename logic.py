30ae9dfcd51e8e78c2406f1332fcae0d0cd811b0
Justin Morravec <justinmoravec97@yahoo.com>
2026-02-03 11:35:33 -0600

    new

---
+++ logic.py
@@ -0,0 +1,12 @@
+"""
+-----------------------------------------------------------------------
+ASSIGNMENT REQUIREMENTS
+-----------------------------------------------------------------------
+[ ] 1. Header Docstring included with your name.
+[ ] 2. Ask user for two integers (num1 and num2).
+[ ] 3. Perform 6 logical checks: (Both > 0, Both > 100, Either Even, Either < 100, Not Equal, Not Zero).
+[ ] 4. Use if/elif/else to categorize num1 (Positive/Negative/Zero).
+[ ] 5. Code is clean and uses descriptive variable names.
+[ ] 6. Upload to GitHub and paste the link below.
+-----------------------------------------------------------------------
+"""
