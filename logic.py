328443fdadde12b775e621429292386c41fa1817
Justin Morravec <justinmoravec97@yahoo.com>
2026-02-03 12:10:03 -0600

    updates

--- logic.py
+++ logic.py
@@ -10,3 +10,42 @@ ASSIGNMENT REQUIREMENTS
 [ ] 6. Upload to GitHub and paste the link below.
 -----------------------------------------------------------------------
 """
+
+# variables
+life_level = int(input("What level are you in life? (0-99)   "))
+age = int(input("How old are you? (0-99) "))
+
+# if/else/elif
+
+if life_level >= 95:
+    level = "You're a Master at life!"
+elif life_level >= 80:
+    level = "You're a Professional at life!"
+elif life_level >= 70:
+    level = "You're a close to success keep going!"
+elif life_level >= 50:
+    level = "Keep working, the success will come!"
+elif life_level >= 30:
+    level = "Everyone starts somewhere, keep on climbing!"
+else:
+    level = "You have to keep climbing to be a master!"
+
+
+if age >= 80:
+    age_description = "You're full of wisdom"
+elif age >= 65:
+    age_description = "You're a senior citizen"
+elif age >= 30:
+    age_description = "You're still a young adult"
+elif age >= 18:
+    age_description = "You're an adult now welcome to the club!"
+elif age >= 13:
+    age_description = "Welcome to your teens, don't blink it goes fast!"
+else:
+    age_description = "Wow, you're so young like a baby!"
+
+
+# outputs
+
+print(f"{level}")
+print(f"{age_description}")
