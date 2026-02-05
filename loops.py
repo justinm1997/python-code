a80a066f81488851c5732be67531432f1eccac3c
Justin Morravec <justinmoravec97@yahoo.com>
2026-02-05 11:49:16 -0600

    update

---
+++ loops.py
@@ -0,0 +1,14 @@
+"""
+-----------------------------------------------------------------------
+ASSIGNMENT REQUIREMENTS
+-----------------------------------------------------------------------
+[ ] 1. Header Docstring included.
+[ ] 2. Task 1: While Loop (The Nagging Kid)
+       - Repeats "Are we there yet?" until user types "yes".
+       - Uses a boolean variable to control the loop.
+[ ] 3. Task 2: For Loop (99 Bottles of Beer)
+       - Counts backwards from 99 to 1.
+       - Prints "[number] bottles of beer on the wall!"
+[ ] 4. Upload to GitHub and paste the link below.
+-----------------------------------------------------------------------
+"""
