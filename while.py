075899411543c134cb9939c1412955dc6b9060b7
Justin Morravec <justinmoravec97@yahoo.com>
2026-02-05 11:42:27 -0600

    updates

--- while.py
+++ while.py
@@ -4,13 +4,14 @@ some while loops
 
 """
 
-while True:
+keep_going = True
+while keep_going:
     print("This is the song that never ends. \n It just goes on and on my friend")
     print("Somebody started singing it not knowing what it was")
     print("and they'll just go on singing it forever just because. ")
 
     somebody_stop_me = input("Keep going? y/n:   ")
     if somebody_stop_me == "y" or somebody_stop_me == "Y":
-        pass
+        keep_going = True
     else:
-        break
+        keep_going = False
