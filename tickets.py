40d4e49b82ad095a4bb2d3c7dfbc0b9d9473de7b
Justin Morravec <justinmoravec97@yahoo.com>
2026-02-17 12:05:50 -0600

    updates

--- tickets.py
+++ tickets.py
@@ -10,3 +10,60 @@ ASSIGNMENT 6A: TICKET SALES
 [ ] 6. Repeat until user quits or seats are empty.
 -----------------------------------------------------------------------
 """
+
+seats_available = [
+    "seat 1",
+    "seat 2",
+    "seat 3",
+    "seat 4",
+    "seat 5",
+    "seat 6",
+    "seat 7",
+    "seat 8",
+    "seat 9",
+    "seat 10",
+    "seat 11",
+    "seat 12",
+    "seat 13",
+    "seat 14",
+    "seat 15",
+    "seat 16",
+    "seat 17",
+    "seat 18",
+    "seat 19",
+    "seat 20",
+]
+
+seats_not_available = [
+    "seat 1",
+    "seat 2",
+    "seat 3",
+    "seat 4",
+    "seat 5",
+    "seat 6",
+    "seat 7",
+    "seat 8",
+    "seat 9",
+    "seat 10",
+    "seat 11",
+    "seat 12",
+    "seat 13",
+    "seat 14",
+    "seat 15",
+    "seat 16",
+    "seat 17",
+    "seat 18",
+    "seat 19",
+    "seat 20",
+]
+
+
+
+
+print(f"\n\nCurrently available seats:  {seats_available} ")
+
+
+int(input("Which seat do you want?(1-20)    "))
+
+
+if choice = seats_available 
