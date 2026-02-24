35e4b28d2981922ffb401c0d411a23f2f9e4c4d1
Justin Morravec <justinmoravec97@yahoo.com>
2026-02-24 11:51:12 -0600

    jtyjt

---
+++ locked_calendar.py
@@ -0,0 +1,43 @@
+"""
+-----------------------------------------------------------------------
+ASSIGNMENT 6B: THE LOCKED CALENDAR
+-----------------------------------------------------------------------
+[ ] 1. Header Docstring included.
+[ ] 2. MONTHS is defined as a constant tuple ().
+[ ] 3. Program uses a for loop to display each month.
+[ ] 4. 'try' and 'except' blocks catch a TypeError.
+[ ] 5. Comments explain why the modification failed.
+-----------------------------------------------------------------------
+"""
+
+MONTHS = (
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+)
+
+for month in MONTHS:
+    print(f"{month}")
+
+    print("\nAttempting to change month...")
+
+try:
+    # Tuples are physically locked. This triggers a TypeError.
+    MONTHS[0] = "Newmonth"  # this will cause a type error
+
+except TypeError:
+    print("Error: System settings are locked and immutable! ")
+
+
+# This assignment fails because tuples are immutable.
+# That means once a tuple is created, its elements cannot be changed,
+# added, or removed. Attempting to assign a new value causes a TypeError.
