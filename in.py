3969b158094360e802799505629fa09bff2f466f
Justin Morravec <justinmoravec97@yahoo.com>
2026-02-26 11:31:09 -0600

    ffasffewg

---
+++ in.py
@@ -0,0 +1,20 @@
+"""
+
+in demo
+
+"""
+
+my_string = "This year feburary is not a leap year."
+
+if "Dr" in my_string:
+    print("yes, individual has a doctorate")
+else:
+    print("No, does not have a doctorate ")
+
+days = ("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday")
+
+
+for day in days:
+    if day.startswith("S")
+
+
