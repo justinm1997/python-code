d9c0767596d254bf483187c917a81ca8679b2bd9
Justin Morravec <justinmoravec97@yahoo.com>
2026-02-26 11:23:40 -0600

    htfdgd

--- string_logic.py
+++ string_logic.py
@@ -3,3 +3,21 @@
 String Logic
 
 """
+
+# testing data
+errors = True
+while errors:
+
+    try:
+        name = input("Please enter your first and last name:  ").title()
+        first_name, last_name = name.split(" ")
+        print(name)
+        print(first_name)
+        print(last_name)
+        print(f"{first_name[0]}{last_name[0]}")
+        errors = False
+    except ValueError as e:
+        print("You have a value error {e}")
+
+    except Exception as e:
+        print(f"This raised an exception {e}")
