c07ddb213ec5bbd4d69033e9f5aaf1beb114c0ea
justinm1997 <140605817+justinm1997@users.noreply.github.com>
2026-03-10 11:38:46 -0500

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,1 +1,44 @@
+"""
+
+functions
+
+"""
+def options():
+    # give options to order, print labels, see history
+
+def menu():
+    print("menu")
+
+def order_system():
+    # take orders from customers 
+    # read in past orders and display
+    # call save function
+    menu()
+    name = input(please enter your name:  )
+    # read file to look for last order 
+    read_orders()
+    print("Order system")
+    save_orders()
+
+def read_orders():
+    # read stored orders
+
+def save_orders():
+    #write to file
+    print("save orders")
+
+def print_labels():
+    #print on every 2 by 3 inch labels
+    print("Print labels")
+
+def calculate_cost():
+    # calculate cost (read in order)
+    print("calculate cost")
+
+def main ():
+    options()
+    order_system()
+    calculate_cost()
+    
+main()
 
