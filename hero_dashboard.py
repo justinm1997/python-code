64499ccf5faf13f532035b0f1ee604c50ab2e9b5
Justin Morravec <justinmoravec97@yahoo.com>
2026-03-31 11:43:19 -0500

    vcxvgg

---
+++ hero_dashboard.py
@@ -0,0 +1,16 @@
+"""
+-----------------------------------------------------------------------
+ASSIGNMENT 11A: THE OFFICE HERO DASHBOARD
+-----------------------------------------------------------------------
+[ ] 1. Header Docstring included.
+[ ] 2. Global constants OFFICE_NAME and TAX_RATE defined in ALL_CAPS.
+[ ] 3. Function 'process_expenses' returns TWO values (float, string).
+[ ] 4. main() function uses try/except for numeric price/qty inputs.
+[ ] 5. main() calls function using KEYWORD ARGUMENTS.
+[ ] 6. main() correctly unpacks and prints both return values.
+-----------------------------------------------------------------------
+"""
+
+TAX_RATE = 0.05
+
+OFFICE_NAME = "Justin's office"
