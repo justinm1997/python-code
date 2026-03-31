c809391b9e2a2970d740dae571a3fb168d95453a
Justin Morravec <justinmoravec97@yahoo.com>
2026-03-31 10:59:48 -0500

    fdgdg

---
+++ pizza_engine.py
@@ -0,0 +1,12 @@
+"""
+-----------------------------------------------------------------------
+ASSIGNMENT 10A: THE RESILIENT PIZZA ENGINE
+-----------------------------------------------------------------------
+[ ] 1. Header Docstring included.
+[ ] 2. Global constant TOPPINGS defined as a Tuple in ALL_CAPS.
+[ ] 3. Function 'make_pizza' defines 4 specific parameters.
+[ ] 4. 'make_pizza' uses a DEFAULT value for is_delivery.
+[ ] 5. main() displays the Global Pantry list to the user.
+[ ] 6. main() calls the function using KEYWORD ARGUMENTS.
+-----------------------------------------------------------------------
+"""
