28cd18d2738ea481222590a088a169dc63ccf021
Justin Morravec <justinmoravec97@yahoo.com>
2026-04-02 11:32:33 -0500

    vdsvhjkj

--- blood_sugar_tracking.py
+++ blood_sugar_tracking.py
@@ -8,32 +8,30 @@ calculated average.... to come
 
 import datetime
 
+
 def get_sugar():
     # 2. Ask the user for their data
     blood_sugar = input("Enter your current blood sugar level: ")
     return blood_sugar
 
+
 def save_to_file(timestamp_str, blood_sugar):
     # 3. Store it in a dictionary using the timestamp as the key
-    health_log = {
-        timestamp_str: blood_sugar
-}
+    health_log = {timestamp_str: blood_sugar}
 
     # 4. Append ('a' mode) the new entry to our text file so it grows over time
     with open("blood_sugar_log.txt", "a") as file:
-        file.write(f"[{timestamp_str}] Blood Sugar: {health_log[timestamp_str]}\n")
-
+        file.write(f"[{timestamp_str}] : {health_log[timestamp_str],}\n")
 
 
 def main():
+    # 1. Get the current time and format it nicely
+    bs = get_sugar()
+    current_time = datetime.datetime.now()
+    timestamp_str = current_time.strftime("%Y-%m-%d %H:%M:%S")
+    save_to_file(timestamp_str, bs)
+    print(f"{timestamp_str} : {bs}")
+    print("Health data successfully logged!")
 
 
-# 1. Get the current time and format it nicely
-
-bs = get_sugar()
-current_time = datetime.datetime.now()
-timestamp_str = current_time.strftime("%Y-%m-%d %H:%M:%S")
-save_to_file(timestamp_str, )
-
-print("Health data successfully logged!")
 main()
