a396bc15796113bdb511df4213b20e7d1228cf7d
Justin Morravec <justinmoravec97@yahoo.com>
2026-04-02 11:35:44 -0500

    khkhjgh

---
+++ menu_creator.py
@@ -0,0 +1,37 @@
+"""
+
+writing to files
+date/time
+create menu text file
+
+"""
+
+import datetime
+
+
+def get_sugar():
+    # 2. Ask the user for their data
+    blood_sugar = input("Enter your current blood sugar level: ")
+    return blood_sugar
+
+
+def save_to_file(timestamp_str, blood_sugar):
+    # 3. Store it in a dictionary using the timestamp as the key
+    health_log = {timestamp_str: blood_sugar}
+
+    # 4. Append ('a' mode) the new entry to our text file so it grows over time
+    with open("blood_sugar_log.txt", "a") as file:
+        file.write(f"[{timestamp_str}] : {health_log[timestamp_str],}\n")
+
+
+def main():
+    # 1. Get the current time and format it nicely
+    bs = get_sugar()
+    current_time = datetime.datetime.now()
+    timestamp_str = current_time.strftime("%Y-%m-%d %H:%M:%S")
+    save_to_file(timestamp_str, bs)
+    print(f"{timestamp_str} : {bs}")
+    print("Health data successfully logged!")
+
+
+main()
