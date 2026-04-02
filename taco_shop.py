15f698de62acdfe637ea39f817971ca7e6566b61
Justin Morravec <justinmoravec97@yahoo.com>
2026-04-02 12:28:32 -0500

    fdgdfgff

--- taco_shop.py
+++ taco_shop.py
@@ -5,30 +5,31 @@ Developer: Justin Moravec
 """
 
 # GLOBAL CONSTANTS (Pantry Rules)
-MENU_FILE = "menu.txt"
+MENU_FILE = "taco_menu"
 
 
 def get_customer_info():
     """Asks for name and office location."""
     # TODO: Ask for name and order number
-    return "Justin Moravec", "Number 101"
+    return "Justin Moravec", "Order Number 1"
 
 
 def take_order():
-    """Collects category, number of tacos, meat, and add ons. Returns data."""
-    # TODO: Capture meat (Chicken/Steak/al pastor/ etc...) and category (Coffee/Tea/Cocoa)
+    """Collects taco category, tortilla, protein, and extras. Returns data."""
+    # TODO: Capture tortilla (Flour/Corn) protein (Beef/Chicken/steak), and category (Taco/Buritto/Nachos)
     pass
 
 
 def calculate_total(order_data):
-    """Calculates price based on taco number and add ons."""
-    # TODO: Load prices from menu.txt
-    return 2.30
+    """Calculates price based on item type and extras."""
+    # TODO: Load prices from taco_menu.txt
+
+    return 7.50
 
 
 def save_data_and_label(customer, total):
     """Appends to order_history.txt and prints the human-readable label."""
-    # TODO: Write raw data for computer and formatted box for cook
+    # TODO: Write raw data for computer and formatted label for kitchen staff
     pass
 
 
