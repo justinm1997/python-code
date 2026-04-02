07293af26121a1e61832e5c8d6beb01ca338f467
Justin Morravec <justinmoravec97@yahoo.com>
2026-04-02 11:53:20 -0500

    jghjgd

--- menu_creator.py
+++ menu_creator.py
@@ -2,36 +2,46 @@
 
 writing to files
 date/time
-create menu text file
+create menu text file for coffee house
 
 """
 
 import datetime
 
 
-def get_sugar():
-    # 2. Ask the user for their data
-    blood_sugar = input("Enter your current blood sugar level: ")
-    return blood_sugar
+def get_menu_options():
+    # get info for menu dictionary
+    menu = {}
+    while True:
+        print("Type Q when done")
+        category = input(
+            "please give me the category for this part of the menu:  "
+        ).upper()
+        if category == "Q":
+            break
+        items = input("please enter items separated by commas: ")
+
+        # add to dictionary
+        menu[category] = items
+
+    print(menu)
+
+    return menu
 
 
-def save_to_file(timestamp_str, blood_sugar):
+def save_to_file(menu):
+    pass
     # 3. Store it in a dictionary using the timestamp as the key
-    health_log = {timestamp_str: blood_sugar}
+    # health_log = {timestamp_str: blood_sugar}
 
     # 4. Append ('a' mode) the new entry to our text file so it grows over time
-    with open("blood_sugar_log.txt", "a") as file:
-        file.write(f"[{timestamp_str}] : {health_log[timestamp_str],}\n")
+    # with open("blood_sugar_log.txt", "a") as file:
+    #    file.write(f"[{timestamp_str}] : {health_log[timestamp_str],}\n")
 
 
 def main():
-    # 1. Get the current time and format it nicely
-    bs = get_sugar()
-    current_time = datetime.datetime.now()
-    timestamp_str = current_time.strftime("%Y-%m-%d %H:%M:%S")
-    save_to_file(timestamp_str, bs)
-    print(f"{timestamp_str} : {bs}")
-    print("Health data successfully logged!")
+    my_menu = get_menu_options()
+    save_to_file(my_menu)
 
 
 main()
