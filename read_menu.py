d8ca5079c8f76c3889981fd0b0ec2ce6f243e1c1
Justin Morravec <justinmoravec97@yahoo.com>
2026-04-07 11:24:02 -0500

    fghfh

--- read_menu.py
+++ read_menu.py
@@ -9,16 +9,30 @@ test reading in our menu and creating list and dictonaries
 
 def read_menu()
     """read menu will import menu.txt and pull it into a list. we can then break down the list into the specific variables and dictonaires that we need """
-    
+    try:
+
+
+    except Exception as e:
+        print(e)
 
 
 
+def split_into_variables(menu_items):
+    """ break the menu file into separate variables """
+
+
+def print_menu(coffee_types, size_and_price, types_of_milk, syrup_flavors, shots)
+"""   will print each menu """
 
 
 
 def main():
     # organize program logic
     menu_items = read_menu()
+    coffee, prices, milks, flavors, shots = split_into_variables(menu_items)
+    print_menu(coffee, prices, milks, flavors, shots)
+
+
 
 
 
