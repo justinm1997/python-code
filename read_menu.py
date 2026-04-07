3f9bbdff93e9ef992f945e3e143c43be9809f62b
Justin Morravec <justinmoravec97@yahoo.com>
2026-04-07 11:16:56 -0500

    rgd

---
+++ read_menu.py
@@ -0,0 +1,26 @@
+"""
+
+test reading in our menu and creating list and dictonaries
+
+"""
+
+
+
+
+def read_menu()
+    """read menu will import menu.txt and pull it into a list. we can then break down the list into the specific variables and dictonaires that we need """
+    
+
+
+
+
+
+
+def main():
+    # organize program logic
+    menu_items = read_menu()
+
+
+
+
+main()
